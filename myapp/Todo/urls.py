98970965317586e6666cc09ee6aa579218c7639d
Absalom <mrclabsalom@gmail.com>
2024-02-06 00:30:36 +0300

    Update to the templates and views

--- myapp/Todo/urls.py
+++ myapp/Todo/urls.py
@@ -4,4 +4,7 @@ from Todo import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('Todos/', views.Todos, name='Todos'),
+    path('edit_task/<int:task_id>/', views.edit_task, name='edit_task'),
+    path('delete_task/<int:task_id>/', views.delete_task, name='delete_task'),
+    path('search/', views.search_tasks, name='search_tasks'),
 ]
--- myapp/Todo/views.py
+++ myapp/Todo/views.py
@@ -1,8 +1,50 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from Todo.models import Tasks
 
 # Create your views here.
 def home(request):
-    return render(request, 'base.html')
+    context = { 'success': False}
+    if request.method == 'POST':
+        # Handling the form
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        print(title, description)
+        ins = Tasks(taskTitle=title, taskDescription=description)
+        ins.save()
+        context = { 'success': True}
+        
+
+    return render(request, 'base.html', context)
 
 def Todos(request):
-    return render(request, 'Todo/tasks.html')
+    allTasks = Tasks.objects.all()
+    context = {'tasks': allTasks}
+    return render(request, 'Todo/tasks.html', context)
+
+def edit_task(request, task_id):
+    task = Tasks.objects.get(id=task_id)
+    context = {'task': task}
+
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        task.taskTitle = title
+        task.taskDescription = description
+        task.save()
+        return redirect('Todos')  # Redirect to Todos view after editing
+
+    return render(request, 'Todo/edit_task.html', context)
+
+def delete_task(request, task_id):
+    task = Tasks.objects.get(id=task_id)
+    task.delete()
+    return redirect('Todos')  # Redirect to Todos view after deletion
+
+def search_tasks(request):
+    query = request.GET.get('q')
+    if query:
+        tasks = Tasks.objects.filter(taskTitle__icontains=query)  # Search by task title (case-insensitive)
+    else:
+        tasks = Tasks.objects.all()
+    context = {'tasks': tasks}
+    return render(request, 'Todo/tasks.html', context)
